3dc71c176244f6cfe403980083974bf752686846
mohan <mohanreddy0532@gmail.com>
2022-01-12 12:14:52 +0530

    terraform sample

--- 01.outputs/sample.tf
+++ 01.outputs/sample.tf
@@ -1,7 +1,11 @@
-output "new" {
+output "New1" {
   value = "Hello Terraform"
 }
 
 output "New2" {
   value = "Hellow Terraform v1.1.3"
+}
+
+output "New3" {
+  value = "Hey Terraform new check3"
 }
