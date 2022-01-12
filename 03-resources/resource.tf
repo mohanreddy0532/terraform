a6fae6918fcb6318f1828e4fc6e5d4dc22e462ac
mohan <mohanreddy0532@gmail.com>
2022-01-12 17:43:37 +0530

    resource sample

--- 03-resources/resource.tf
+++ 03-resources/resource.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "sample" {
-  #ami = "ami-0974e63a6f2a1aed7"
   ami = "ami-04656078adf4aa403"
   instance_type = "t2.micro"
 }
 
 output "out" {
-  value = aws_instance.sample
+  value = aws_instance.sample.public_ip
 }
