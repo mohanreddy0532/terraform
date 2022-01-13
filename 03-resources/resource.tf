056cdaf6947e385f1a23050683babc9e6bdde198
mohan <mohanreddy0532@gmail.com>
2022-01-13 12:43:01 +0530

    resource sample

--- 03-resources/resource.tf
+++ 03-resources/resource.tf
@@ -1,8 +1,33 @@
 resource "aws_instance" "sample" {
-  ami = "ami-04656078adf4aa403"
+  ami           = "ami-04656078adf4aa403"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
 output "out" {
   value = aws_instance.sample.public_ip
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
