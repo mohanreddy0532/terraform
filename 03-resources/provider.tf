874bf39f1d186aa33a090df8f1d92a031a4bf820
mohan <mohanreddy0532@gmail.com>
2022-01-13 13:09:31 +0530

    state file s3

--- 03-resources/provider.tf
+++ 03-resources/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-s3-mohan"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
