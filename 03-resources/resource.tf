607e4e51c87fd1a002400aa571c53238557d341e
mohan <mohanreddy0532@gmail.com>
2022-02-22 17:29:44 +0530

    tf resource ami

--- 03-resources/resource.tf
+++ 03-resources/resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami           = "ami-04656078adf4aa403"
+  ami           = "ami-0d997c5f64a74852c"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
