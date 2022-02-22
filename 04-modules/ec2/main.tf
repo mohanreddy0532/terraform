97d3575916bcef86d69442b5bf0e0a1076c6fd24
mohan <mohanreddy0532@gmail.com>
2022-02-22 22:41:36 +0530

    tf ec2 id

--- 04-modules/ec2/main.tf
+++ 04-modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami                    = "ami-0974e63a6f2a1aed7"
+  ami                    = "ami-0d997c5f64a74852c"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
 }
